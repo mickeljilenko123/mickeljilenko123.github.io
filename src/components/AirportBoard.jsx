16b3ff911c25bcc27d3281273a69384b966f18de
mickeljilenko123 <mickeljilenko@gmail.com>
2021-04-08 00:23:01 +0300

    init

---
+++ src/components/AirportBoard.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import moment from "moment";
+import SearchFlights from "./SearchFlights";
+import FlightSchedule from "./FlightSchedule";
+import * as flightsActions from "../redux/flight.actions";
+
+const AirportBoard = ({ fetchFlightsList }) => {
+  useEffect(() => {
+    fetchFlightsList(moment().format("DD-MM-YYYY"));
+  }, [fetchFlightsList]);
+
+  return (
+    <div>
+      <SearchFlights />
+      <FlightSchedule />
+    </div>
+  );
+};
+
+
+const mapDispatch = {
+  fetchFlightsList: flightsActions.fetchFlightsList,
+};
+
+export default connect(null, mapDispatch)(AirportBoard);
